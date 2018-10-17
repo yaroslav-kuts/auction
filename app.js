2f911f6a4bf7023f393be500df55c344928a2cbf
zzzonked <yaroslavkuts@gmail.com>
2018-10-17 13:04:28 +0300

    Graceful shutdown was implemented.

--- app.js
+++ app.js
@@ -1,12 +1,26 @@
-var express = require('express');
+const express = require('express');
+const db = require('./models/db');
 
-var app = express();
+const app = express();
+
+const close = (err, onclose) => {
+  if (onclose) onclose();
+  if (err) console.log(err.message);
+  db.connection.close();
+  process.exit();
+};
 
 app.get('/api/healthcheck', function (req, res) {
   res.status(200);
   res.json({ status: 'OK' });
 });
 
-app.listen(3000, function () {
+app.listen(3001, function () {
   console.log('Auction app listening on port 3000!');
+});
+
+process.on('SIGINT', function() {
+  close(null, () => {
+    console.log('\nProcess was interupted!');
+  });
 });
