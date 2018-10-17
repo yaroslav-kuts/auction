f07c3d2962a6875d99fa2805d10771e24c4ce17c
zzzonked <yaroslavkuts@gmail.com>
2018-10-17 14:50:35 +0300

    Added before() function that cleans user collection.

--- tests/user.test.js
+++ tests/user.test.js
@@ -1,13 +1,21 @@
 const assert = require('chai').assert;
 const users = require('../models/users');
+const mongoose = require('../models/db');
 
-const userok = { email: 'some199@email.com',
-                 phone: '+1-541-854-3036',
+const userok = { email: 'some22199@email.com',
+                 phone: '+1-541-854-3176',
                  firstName: 'John',
                  lastName: 'Dou',
                  birthday: '2018-10-16T09:31:44.992Z' };
 
+
 describe('User', function() {
+
+  before(async function() {
+    await users.truncate();
+    console.log('done!');
+  });
+
   describe('#create()', function() {
     it('should add one new user to DB', function(done) {
       let numOfUsers = users.count();
