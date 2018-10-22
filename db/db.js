b787b7e4faee67212285b5da4f1246f3f4ade27a
zzzonked <yaroslavkuts@gmail.com>
2018-10-22 17:48:15 +0300

    MongoDB pool size value was moved to the config file.

--- db/db.js
+++ db/db.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
+const config = require('../config/config');
 
-const options = { poolSize: 10, useNewUrlParser: true };
+const options = { poolSize: config.poolSize, useNewUrlParser: true };
 
 mongoose.connect(process.env.DB_URI, options);
 
